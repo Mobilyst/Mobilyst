43fe9d52a4b95decddec1b20f54017f166b4f799
Oktay Tultar <109659197+OktayTltr@users.noreply.github.com>
2023-06-13 21:17:34 +0300

    Kategori Sayfası
    Kategori sayfasının kodları yazıldı ve sayfa içinde arama yapabilmek için bir 'Arama' butonu eklendi bununla birlikte kategori sayfasından yemek çeşitlerine gidip aynı şekilde sayfa içinde arama yapabilmek için kodlar ve butonlar da eklendi.

---
+++ lib/kategoriler_ekrani/cesitlerEkrani.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:mobilyst/verilerRepository.dart';
+import 'package:mobilyst/yemekKategori.dart';
+
+
+class CesitlerEkrani extends StatefulWidget {
+  const CesitlerEkrani({super.key, required this.kategori});
+
+  final YemekKategorisi kategori;
+
+  @override
+  State<CesitlerEkrani> createState() => _CesitlerEkraniState();
+}
+
+class _CesitlerEkraniState extends State<CesitlerEkrani> {
+  List<YemekCesiti> newList = [];
+  List<YemekCesiti> cesitler = [];
+  TextEditingController editingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (var i = 0; i < VerilerRepository().cesitler.length; i++) {
+      if (VerilerRepository().cesitler[i].kategoriAdi ==
+          widget.kategori.kategoriAdi) {
+        cesitler.add(VerilerRepository().cesitler[i]);
+      }
+    }
+    newList.addAll(cesitler);
+  }
+
+  void kategoriAra(String text){
+    if (text.isEmpty) {
+      newList.clear();
+      cesitler.forEach((element) {
+        newList.add(element);
+      });
+      setState(() {});
+      return;
+    }
+    newList.clear();
+    for (var i = 0; i < cesitler.length; i++) {
+      if (cesitler[i].cesitAdi.toLowerCase().contains(text.toLowerCase())) {
+        newList.add(cesitler[i]);
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    editingController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(onPressed: () {
+            Navigator.pop(context);
+          }, icon: Icon(Icons.arrow_back_outlined,color: Colors.black,)),
+          title: SizedBox(
+            height: 60,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (text) {
+                  kategoriAra(text);
+                },
+                controller: editingController,
+                decoration: const InputDecoration(
+                  labelText: '',
+                  suffixIcon: Icon(
+                    Icons.search,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(widget.kategori.kategoriAdi+" Kategorisi",style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(newList[index].cesitAdi),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+
+                  onTap: () {
+
+                  },
+                );
+              },
+              itemCount: newList.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/kategoriler_ekrani/verilerRepository.dart
@@ -0,0 +1,23 @@
+
+import 'package:mobilyst/yemekKategori.dart';
+
+class VerilerRepository {
+  List<YemekKategorisi> kategoriler = [
+    YemekKategorisi(
+        kategoriAdi: 'Pizza',
+        kategoriResimUrl:
+        'https://cdn-icons-png.flaticon.com/128/1404/1404945.png'),
+    YemekKategorisi(
+        kategoriAdi: 'Hamburger',
+        kategoriResimUrl:
+        'https://cdn-icons-png.flaticon.com/128/3075/3075929.png')
+  ];
+
+  List<YemekCesiti>cesitler=[
+    YemekCesiti(kategoriAdi: 'Pizza', cesitAdi: 'Salamlı Pizza'),
+    YemekCesiti(kategoriAdi: 'Pizza', cesitAdi: 'Peynirli Pizza')
+
+
+  ];
+
+}
