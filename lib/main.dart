6a5589291a9acf3d982bf0ce3bee3e73f5f0b88f
emelacarr <129789689+emelacarr@users.noreply.github.com>
2023-07-12 01:31:14 +0300

    logo tamamlandı

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-
-import 'food_comparison_screen/food_comparison_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +10,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(
+        children: [
+          Image.asset(
+            'assets/logo.png.gif',
+            height: 75,
+          ),
+        ],
+      ),
+      backgroundColor: Color.fromARGB(255, 254, 115, 28),
+      nextScreen: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
     );
   }
 }
