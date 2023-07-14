1d2ab620bda3078570b166caf43da2db1d85fe6c
Oktay Tultar <109659197+OktayTltr@users.noreply.github.com>
2023-07-15 01:08:52 +0300

    .

--- lib/food_comparison_screen/food_comparison_screen.dart
+++ lib/food_comparison_screen/food_comparison_screen.dart
@@ -20,121 +20,121 @@ class YemekKiyasTumuPage extends ConsumerWidget {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Yemekler",
-          style: TextStyle(
-            color: AppColors.bir,
+        appBar: AppBar(
+          title: const Text(
+            "Yemekler",
+            style: TextStyle(
+              color: AppColors.bir,
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,
+          centerTitle: true,
+          backgroundColor: AppColors.uc,
         ),
-        centerTitle: true,
-        backgroundColor: AppColors.uc,
-      ),
-      body: GridView(
-        shrinkWrap: true,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 15,
-          childAspectRatio: 0.62,),
-        children: List.generate(
-          meals.length,
-              (int index) {
-            final mealList = meals[index];
-            return ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              //   crossAxisCount: 2,
-              //   mainAxisSpacing: 5,
-              //   crossAxisSpacing: 15,
-              //   childAspectRatio: 0.62,
-              // ),
-              itemBuilder: (BuildContext context, int mealIndex) {
-                // İndeks ismini mealIndex olarak değiştirin
-                final urun = mealList[mealIndex]; // Doğru indeksi kullanın
-                return Card(
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.grey,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FoodComparisonScreen(yemek: urun),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: double.infinity,
-                      height: 280,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 1,
-                        ),
-                        borderRadius: BorderRadius.circular(5),
+        body: GridView(
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 15,
+            childAspectRatio: 0.62,),
+          children: List.generate(
+            meals.length,
+                (int index) {
+              final mealList = meals[index];
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                //   crossAxisCount: 2,
+                //   mainAxisSpacing: 5,
+                //   crossAxisSpacing: 15,
+                //   childAspectRatio: 0.62,
+                // ),
+                itemBuilder: (BuildContext context, int mealIndex) {
+                  // İndeks ismini mealIndex olarak değiştirin
+                  final urun = mealList[mealIndex]; // Doğru indeksi kullanın
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.grey,
+                        width: 1.0,
                       ),
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 5),
-                            child: SizedBox(
-                              child: Image.network(
-                                urun.image_url,
-                                fit: BoxFit.cover,
-                                height: 200,
-                                width: 170,
-                              ),
-                            ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FoodComparisonScreen(yemek: urun),
                           ),
-                          SizedBox(
-                            height: 1,
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 280,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1,
                           ),
-
-                          Text(
-                            urun.name,
-                            maxLines: 2,
-                            textAlign: TextAlign.start,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 1,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Fiyat: ${urun.price}",
-                                textAlign: TextAlign.start,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                  color: AppColors.bes,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: SizedBox(
+                                child: Image.network(
+                                  urun.image_url,
+                                  fit: BoxFit.cover,
+                                  height: 200,
+                                  width: 170,
                                 ),
                               ),
-                            ],
-                          ),
-                        ],
+                            ),
+                            SizedBox(
+                              height: 1,
+                            ),
+
+                            Text(
+                              urun.name,
+                              maxLines: 2,
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 1,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Fiyat: ${urun.price}",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: AppColors.bes,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-              itemCount: mealList.length,
-            );
-          },
-        ),
-      )
+                  );
+                },
+                itemCount: mealList.length,
+              );
+            },
+          ),
+        )
 
     );
   }
