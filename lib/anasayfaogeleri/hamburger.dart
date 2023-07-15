8f7393266c52a9e1d6c1d288d1d7f78dc0d2efa8
Mobilyst <deryatt25@gmail.com>
2023-07-15 23:38:11 +0300

    categorylere gore urunler ayrıldı

---
+++ lib/anasayfaogeleri/hamburger.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobilyst/ColorAndType/color.dart';
+import 'package:mobilyst/food_comparison_screen/UrunRepository.dart';
+import 'package:mobilyst/food_comparison_screen/food_bilgileri.dart';
+
+import '../oktay/OktayKarsilastirma/karsilastirma_ekrani/yemekkarsilastirmaekrani.dart';
+
+class TatliTumuPage extends ConsumerWidget {
+  const TatliTumuPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final urunRepository = ref.watch(urunProvider);
+    final List<List<products>> meals = urunRepository.urunler;
+    final bool isFetching = urunRepository.isFetching;
+
+    if (isFetching) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    final List<products> filteredMeals = meals
+        .expand((mealList) => mealList)
+        .where((urun) => urun.category.toLowerCase() == "hambuger")
+        .toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Hamburgerler",
+          style: TextStyle(
+            color: AppColors.bir,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.uc,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: DropdownButton<SiralamaSecenekleri>(
+              onChanged: (SiralamaSecenekleri? newValue) {
+                if (newValue == SiralamaSecenekleri.PriceDescending) {
+                  filteredMeals.sort((a, b) => a.price.compareTo(b.price));
+                } else if (newValue == SiralamaSecenekleri.PriceAscending) {
+                  filteredMeals.sort((a, b) => b.price.compareTo(a.price));
+                }
+              },
+              items: const [
+                DropdownMenuItem(
+                  value: SiralamaSecenekleri.Sirala,
+                  child: Text("Önerilen Sıralama"),
+                ),
+                DropdownMenuItem(
+                  value: SiralamaSecenekleri.PriceDescending,
+                  child: Text("Fiyata Göre Azalan"),
+                ),
+                DropdownMenuItem(
+                  value: SiralamaSecenekleri.PriceAscending,
+                  child: Text("Fiyata Göre Artan"),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 15,
+                childAspectRatio: 0.62,
+              ),
+              itemCount: filteredMeals.length,
+              itemBuilder: (BuildContext context, int index) {
+                final urun = filteredMeals[index];
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              FoodComparisonScreen(yemek: urun),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 280,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 5),
+                            child: SizedBox(
+                              child: Image.network(
+                                urun.image_url,
+                                fit: BoxFit.cover,
+                                height: 200,
+                                width: 170,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 1,
+                          ),
+                          Text(
+                            urun.name,
+                            maxLines: 2,
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 1,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Fiyat: ${urun.price} TL",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: AppColors.bes,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/food_comparison_screen/food_bilgileri.dart
+++ lib/food_comparison_screen/food_bilgileri.dart
@@ -1,11 +1,18 @@
 class products {
   String productsId;
+  String category;
   String image_url;
   String name;
   dynamic price;
   String product_url;
   products(
-      this.productsId, this.image_url, this.name, this.price, this.product_url);
+    this.productsId,
+    this.category,
+    this.image_url,
+    this.name,
+    this.price,
+    this.product_url,
+  );
 }
 
 enum SiralamaSecenekleri {
